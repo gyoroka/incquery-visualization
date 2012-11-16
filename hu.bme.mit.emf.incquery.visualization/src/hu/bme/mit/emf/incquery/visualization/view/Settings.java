148bbf866bcffd4a59ee64aaad2430cdc60f580c
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2012-11-16 14:57:50 +0100

    Updated color theme

--- hu.bme.mit.emf.incquery.visualization/src/hu/bme/mit/emf/incquery/visualization/view/Settings.java
+++ hu.bme.mit.emf.incquery.visualization/src/hu/bme/mit/emf/incquery/visualization/view/Settings.java
@@ -13,10 +13,10 @@ public class Settings {
 		public static Color tempNodeBackground=new Color(Display.getDefault(),175,175,175);
 		public static Color tempNodeForeground=new Color(Display.getDefault(),255,255,255);
 		
-		public static Color find=new Color(Display.getDefault(),100,100,100);
-		public static Color findForeground=new Color(Display.getDefault(),220,220,250);
-		public static Color findNeg=new Color(Display.getDefault(),255, 0, 0);
-		public static Color findNegForeground=new Color(Display.getDefault(),255, 200, 200);
+        public static Color find = new Color(Display.getDefault(), 55, 112, 231);
+        public static Color findForeground = new Color(Display.getDefault(), 255, 255, 255);
+        public static Color findNeg = new Color(Display.getDefault(), 127, 0, 77);
+        public static Color findNegForeground = new Color(Display.getDefault(), 255, 255, 255);
 		
 		public static Color defaultRelHighlight=new Color(Display.getDefault(),250, 250, 50);
 		public static Color defaultRel=new Color(Display.getDefault(),50,50,100);
